102305d8c1004482010924f302b393cfc4552752
Rajith.D Wickramasinghe <rjgraphic.lanka@gmail.com>
2025-05-09 21:35:02 +0530

    Add files via upload

---
+++ src/java/cinema/Payments.java
@@ -0,0 +1,13 @@
+package cinema.model;
+
+public class Payments {
+    private int customerId;
+    private String filmName;
+    private String showTime;
+    private String seats;
+
+    public void setCustomerId(int customerId) { this.customerId = customerId; }
+    public void setFilmName(String filmName) { this.filmName = filmName; }
+    public void setShowTime(String showTime) { this.showTime = showTime; }
+    public void setSeats(String seats) { this.seats = seats; }
+}

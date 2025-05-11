8a5e7aa1dc8e587f8e21c044473ecaf69a58d9dd
Rajith.D Wickramasinghe <rjgraphic.lanka@gmail.com>
2025-05-11 19:31:05 +0530

    Update Payments.java

--- src/java/cinema/model/com/Payments.java
+++ src/java/cinema/model/com/Payments.java
@@ -10,59 +10,90 @@ package cinema.model.com;
  */
 
 public class Payments {
-    private int id;
+    private int customerId;
     private String cardName;
     private String cardNumber;
     private String expiryDate;
     private String cvv;
+    private String filmName;
+    private String showTime;
+    private String seats;
     private double amount;
 
-    // Getters & Setters
-    public int getId() {
-        return id;
-    }
+    // Constructor
+    public Payments() {}
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCardName() {
-        return cardName;
+    // Setters
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
     }
 
     public void setCardName(String cardName) {
         this.cardName = cardName;
     }
 
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
     public void setCardNumber(String cardNumber) {
         this.cardNumber = cardNumber;
-    }
-
-    public String getExpiryDate() {
-        return expiryDate;
     }
 
     public void setExpiryDate(String expiryDate) {
         this.expiryDate = expiryDate;
     }
 
-    public String getCvv() {
-        return cvv;
-    }
-
     public void setCvv(String cvv) {
         this.cvv = cvv;
     }
 
-    public double getAmount() {
-        return amount;
+    public void setFilmName(String filmName) {
+        this.filmName = filmName;
+    }
+
+    public void setShowTime(String showTime) {
+        this.showTime = showTime;
+    }
+
+    public void setSeats(String seats) {
+        this.seats = seats;
     }
 
     public void setAmount(double amount) {
         this.amount = amount;
+    }
+
+    // Getters
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public String getFilmName() {
+        return filmName;
+    }
+
+    public String getShowTime() {
+        return showTime;
+    }
+
+    public String getSeats() {
+        return seats;
+    }
+
+    public double getAmount() {
+        return amount;
     }
 }
